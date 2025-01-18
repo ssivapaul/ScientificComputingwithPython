05aa1b4b76f4305fed9aa12632f056faaa2e7b34
Siva Sivapalan <ssivapaul@gmail.com>
2025-01-18 20:11:16 +1100

    Second commit

--- BuildingAProjectileTrajectoryCalculator/TrajectoryCalculator.py
+++ BuildingAProjectileTrajectoryCalculator/TrajectoryCalculator.py
@@ -16,9 +16,9 @@ class Projectile:
     def __str__(self):
         return f'''
 Projectile details:
-speed: {self.__speed} m/s
-height: {self.__height} m
-angle: {round(math.degrees(self.__angle))}°
+speed: {self.speed} m/s
+height: {self.height} m
+angle: {self.angle}°
 displacement: {round(self.__calculate_displacement(), 1)} m
 '''
 
@@ -34,21 +34,58 @@ displacement: {round(self.__calculate_displacement(), 1)} m
     def __calculate_y_coordinate(self, x):
         height_component = self.__height
         angle_component = math.tan(self.__angle) * x
-        acceleration_component = GRAVITATIONAL_ACCELERATION * x**2 / (
-                2 * self.__speed**2 * math.cos(self.__angle)**2)
+        acceleration_component = GRAVITATIONAL_ACCELERATION * x ** 2 / (
+                2 * self.__speed ** 2 * math.cos(self.__angle) ** 2)
         y_coordinate = height_component + angle_component - acceleration_component
 
         return y_coordinate
-
+    
     def calculate_all_coordinates(self):
-        xy = []
-        displacement = self.__calculate_displacement()
-        displacement = math.ceil(displacement)
-        for x in range(displacement):
-            y = (self.__calculate_y_coordinate(x))
-            xy.append((x,y))
-        return xy
+        return [
+            (x, self.__calculate_y_coordinate(x))
+            for x in range(math.ceil(self.__calculate_displacement()))
+        ]
+
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def angle(self):
+        return round(math.degrees(self.__angle))
+
+    @property
+    def speed(self):
+        return self.__speed
+
+    @height.setter
+    def height(self, n):
+        self.__height = n
+
+    @angle.setter
+    def angle(self, n):
+        self.__angle = math.radians(n)
+
+    @speed.setter
+    def speed(self, s):
+       self.__speed = s
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.speed}, {self.height}, {self.angle})"
+
+class Graph:
+    
+    __slots__ = ('__coordinates')
+    def __init__(self, coordinates):
+        self.__coordinates = coordinates
         
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self.__coordinates})'
+
 ball = Projectile(10, 3, 45)
+print(ball)
 coordinates = ball.calculate_all_coordinates()
-print(coordinates)
+graph = Graph(coordinates)
+print(graph._Graph__coordinates)
+print("\n")
+print(repr(graph))
